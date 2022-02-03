3ed6a6f1171553086066ec6af343d87813b19ad5
Michael Pinelli <michael.pinelli@mymail.champlain.edu>
2022-02-03 13:31:32 -0500

    Week 3

---
+++ Week03/class/urlHaus.py
@@ -0,0 +1,26 @@
+#Added import for re,
+import csv, re
+#Was ur1HausOpen changed 1 to l
+def urlHausOpen(filename,searchTerm):
+    #Changed formatting also changed while to with, csv.review to csv.reader
+    with open(filename) as f:
+        contents = csv.reader(f, delimiter=' ')
+    for _ in range(9):
+        next(contents)
+    #Was searchTerms removed the s
+    for keyword in searchTerm:
+        
+        for eachLine in contents:
+                #Fixed formatting
+                x = re.findall(r''+keyword+'', eachLine[2])
+                
+                for _ in x:
+        # Don't edit this line. It is here to show how it is possible
+        # to remove the "tt" so programs don't convert the malicious
+        # domains to links that an be accidentally clicked on.
+                    the_url = eachLine[2].replace("http","hxxp")
+                    the_src = eachLine[4]
+                    print("""
+                    URL:
+                    Info: 
+                    {}""",format(the_url, the_src,"*"+60))
