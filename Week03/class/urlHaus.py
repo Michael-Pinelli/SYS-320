5e8e981a0f84abbb2341ec8bb635d13257deda18
Michael Pinelli <michael.pinelli@mymail.champlain.edu>
2022-02-03 22:56:30 -0500

    Week 3

--- Week03/class/urlHaus.py
+++ Week03/class/urlHaus.py
@@ -1,17 +1,18 @@
 #Added import for re
 import csv, re
 #Was ur1HausOpen changed 1 to l
-def urlHausOpen(filename,searchTerm):
-    #Changed formatting also changed while to with, csv.review to csv.reader
-    with open(filename) as f:
+def urlHausOpen(filename, searchTerm):
+    #Changed formatting also changed while to with, csv.review to csv.reader, made read-only
+    with open(filename, 'r') as f:
         contents = csv.reader(f)
+        
         for _ in range(9):
             next(contents)
-        #Was searchTerms removed the s
-        for keyword in searchTerm:
-    
-            for eachLine in contents:
-                    #Fixed formatting
+        #Changed first parameter to eachLine and second one to contents
+        for eachLine in contents:
+            #Changed first parameter to keyword and second one to searchTerm
+            for keyword in searchTerm:
+                    #Fixed formatting, changed keyword to searchTerm
                     x = re.findall(r''+keyword+'', eachLine[2])
 
                     for _ in x:
@@ -19,9 +20,11 @@ def urlHausOpen(filename,searchTerm):
         # to remove the "tt" so programs don't convert the malicious
         # domains to links that an be accidentally clicked on.
                         the_url = eachLine[2].replace("http","hxxp")
-                        the_src = eachLine[4]
+                        #Changed 6 to 7 for the non test data csv
+                        the_src = eachLine[7]
                         print("""
-                        URL:
-                        Info: 
-                        {}""".format(the_url, the_src, 60))
-                         #Removed the * and made it just 60
+                        URL:{}
+                        Info:{}
+                        ************************************************
+                        """.format(the_url, the_src))
+                        #Removed the "*"+60 and the {} on the format line, added {} after url and info
